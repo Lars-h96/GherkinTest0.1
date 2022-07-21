26c019c1c2460dc76510183f9beeb702fef6df83
Lars-h96 <109527133+Lars-h96@users.noreply.github.com>
2022-07-21 16:45:39 +0200

    Added the webdriver

--- UI/src/main/java/webdriver/Webdriver.java
+++ UI/src/main/java/webdriver/Webdriver.java
@@ -2,6 +2,9 @@ package webdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import webdriver.config.*;
 
-public class Webdriver{
+import java.util.List;
+import java.util.Set;
+
+public class Webdriver implements WebDriver {
 
     private RemoteWebDriver driver;
 
@@ -50,5 +56,70 @@ public class Webdriver{
         if (uiConfig.isMaximize()) {
             driver.manage().window().maximize();
         }
+    }
+
+    @Override
+    public void get(String url) {
+
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public String getPageSource() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return null;
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return null;
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return null;
+    }
+
+    @Override
+    public Navigation navigate() {
+        return null;
+    }
+
+    @Override
+    public Options manage() {
+        return null;
     }
 }
